3db278057bbf590b28c4ee3d90ededf42ad54e0c
Mr Lajawab <108144559+MrLajawab19@users.noreply.github.com>
2024-11-10 09:22:02 +0000

    variables

---
+++ basics_java/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234567890;
+let accountHolder = "Ayush Bardhani";
+accountMail = "Ayush@gmail.com"
+
+// const is for integers 
+// let is for variables
+console.log(accountId);
+console.log(accountHolder);
+console.log(accountMail);
+
+console.table([accountId,accountHolder,accountMail]);
+
+
