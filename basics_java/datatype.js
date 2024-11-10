0c06cbc3cb96b55ea08442463c2747c1ab58b4c0
Mr Lajawab <108144559+MrLajawab19@users.noreply.github.com>
2024-11-10 14:37:57 +0000

    datatypes and conversion

---
+++ basics_java/datatype.js
@@ -0,0 +1,15 @@
+let name = "Ayush"//String
+let age = 18 //number
+let isMale = true
+//symbol unique
+//undefined
+//null
+//Boolean true or false
+//
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isMale)
+
+//Type conversion
+let name2 = Number(name)
+console.log(typeof name2)
