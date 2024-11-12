c41683b29b6952bc7bbaa13fa25d7aa3b8f60c96
Mr Lajawab <108144559+MrLajawab19@users.noreply.github.com>
2024-11-12 03:58:14 +0000

    number to string problems

--- basics_java/datatype.js
+++ basics_java/datatype.js
@@ -13,3 +13,10 @@ console.log(typeof isMale)
 //Type conversion
 let name2 = Number(name)
 console.log(typeof name2)
+
+///Conversion from string to number or number to string
+let a = 20
+let b = "3"
+console.log(a+b)
+console.log(1+1+"2")     //1 + 1 =2 then 2and 2 = 22
+console.log("1"+1+2)     //"1" treated as a string and then the whole operation will work as default in string
