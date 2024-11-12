a21ad2c94f629fd469404bcaf7b1b3ea3887a520
Mr Lajawab <108144559+MrLajawab19@users.noreply.github.com>
2024-11-12 04:39:15 +0000

    summary

---
+++ basics_java/datatypes2.js
@@ -0,0 +1,33 @@
+//2 type of datatypes
+//Primitive 7 and reference 3
+//Primitive = String, Number, null, Undefined, Boolean, BigInt,
+//Non primitive = Arrays, Objects, Function
+
+let name = "Ayush"
+let age = 20
+let temperature =null
+let salary;
+let isSingle = true
+let networth = 1239123456789123456789n
+let id = Symbol("123")
+
+
+let matrix = ["1","2","3"]
+let myObj = {
+    name: "Ayush",
+    age: "20"
+}
+const myfunc = function () {
+    console.log("Hello Ayush");
+    
+    
+}
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof temperature);
+console.log(typeof salary);
+console.log(typeof isSingle);
+console.log(typeof networth);
+console.log(typeof id);
+
