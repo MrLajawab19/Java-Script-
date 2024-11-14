38beab0a7d21486dc6420bbdaa161c8b35d8987f
Mr Lajawab <108144559+MrLajawab19@users.noreply.github.com>
2024-11-14 08:30:24 +0000

    Stack and Heap

---
+++  basics_JS/Stack&Heap.js
@@ -0,0 +1,8 @@
+//Stack - Primitive Datatypes are used in this . In this copy of the original value is taken. No change to original value.
+//Heap - Non primitive datatypes are used in this. In this reference of original value is taken meaning changes takes place in original value.
+
+let name = "Ayush Bardhani"
+let name2 = name
+name2 = "Rishi Bardhani"
+console.log("name")
+console.log("name2")  
